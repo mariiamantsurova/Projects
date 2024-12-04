9726e6d4d88802cd2c4a30746e572c71e506a721
Michelle Rudinsky <michellerudinsky@Michelles-MacBook-Air.local>
2024-12-04 20:57:26 +0200

    added the operation using numpy and changed a bit the order of the conditons

--- Matrix-Calculator/ex1_q1.py
+++ Matrix-Calculator/ex1_q1.py
@@ -1,8 +1,9 @@
-from flask import Flask, redirect, request, render_template
+from flask import Flask, redirect, request, render_template, flash
+import math
 import numpy as np
 
 app = Flask(__name__)
-
+app.secret_key = 'your_secret_key_here'
 @app.route('/')
 def homepage():
     return render_template("main_page.html")
@@ -17,28 +18,29 @@ def calculate():
 
     # Check for all required variables
     if not operation or not mat1 or not mat2:
-        return "Missing parameters"
-        #### url to homepage
+        flash("Missing parameters")
+        return redirect("/") #### redirecting  to homepage
 
     # operation validation
     if not operation.lower() in ('multiply', 'add'):
-        return 'Operation is not valid'
-        #### url to homepage
+        flash('Operation is not valid')
+        return redirect("/") #### redirecting to homepage
 
     # matrix validation
     try:
         mat1 = mat1.split(',')
         mat2 = mat2.split(',')
 
-        # matrix's must be with the same size
-        if len(mat1) != len(mat2):
-            return 'Invalid format.<br>Please provide square matrix\'s and in the same size, contain only integers separated by commas'
-            #### url to homepage
-
         # matrix's must be square
-        elif len(mat1) % 2 != 0:
-            return 'Invalid format.<br>Please provide square matrix\'s and in the same size, contain only integers separated by commas'
-            #### url to homepage
+        # if the sqrt(mat) is int then it's a quadric mat
+        if not math.sqrt(len(mat1)).is_integer():
+            flash('Invalid format.<br>Please provide square matrix\'s and in the same size, contain only integers separated by commas')
+            return redirect("/")
+
+        # matrix's must be with the same size
+        elif len(mat1) != len(mat2):
+            flash('Invalid format.<br>Please provide square matrix\'s and in the same size, contain only integers separated by commas')
+            return redirect("/")  #### url to homepage
 
         # matrix's must contain only int
         else:
@@ -47,14 +49,37 @@ def calculate():
                 mat1 = list(map(int, mat1))
                 mat2 = list(map(int, mat2))
             except:
-                return 'Invalid format.<br>Please provide square matrix\'s and in the same size, contain only integers separated by commas'
-                #### url to homepage
+                flash('Invalid format.<br>Please provide square matrix\'s and in the same size, contain only integers separated by commas')
+                return redirect("/")
 
     except:
-        return 'Invalid format.<br>Please provide square matrix\'s and in the same size, contain only integers separated by commas'
-        #### url to homepage
+        flash('Invalid format.<br>Please provide square matrix\'s and in the same size, contain only integers separated by commas')
+        return redirect("/")
 
-    return render_template("calculator.html")
+    # Determine the size of the square matrix
+    n = int(math.sqrt(len(mat1)))
+
+    # Reshape the flat lists into matrix
+    mat1 = np.array(mat1).reshape(n, n)
+    mat2 = np.array(mat2).reshape(n, n)
+
+    # matrix multiplication
+    if operation == "multiply":
+        result = np.dot(mat1, mat2)  # Or equivalently, result = mat1 @ mat2
+    elif operation == "add":
+        result = mat1 + mat2
+    # Perform the operation
+    if operation.lower() == "multiply":
+        result = np.dot(mat1, mat2)
+        operation_type = "Multiplication"
+    elif operation.lower() == "add":
+        result = mat1 + mat2
+        operation_type = "Addition"
+
+    # Render the result page
+    return render_template("calculator.html", result=result.tolist(), operation=operation_type)
+
+    #return render_template("calculator.html")
 
 
 @app.errorhandler(404)  # Handle 404 errors - in case of page do not exist
