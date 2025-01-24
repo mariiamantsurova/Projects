1e3e1186d8687f76fc8793ea1236930db4828db5
Michelle Rudinsky <michellerudinsky@Michelles-MacBook-Air.local>
2025-01-24 09:36:27 +0200

    added queries of visualizations

--- Online_store/sql files/queries.sql
+++ Online_store/sql files/queries.sql
@@ -21,8 +21,29 @@ UNION ALL
 )
 ORDER BY category ASC
 ;
+-- report 3.2
+-- costumers' behavioral purchase by categories
+WITH most_frequent AS (SELECT CIT.sku
+						FROM online_store.clothes_in_transaction AS CIT
+                        GROUP BY CIT.sku
+                        HAVING SUM(CIT.amount) > (SELECT AVG(total_amount)
+													FROM (SELECT SUM(CIT.amount) AS total_amount
+															FROM online_store.clothes_in_transaction AS CIT
+                                                            GROUP BY CIT.sku
+                                                            )
+                                                            AS subquery))
 
-
+SELECT users.faculty, users.age, users.sex,
+        COUNT(DISTINCT(T.order_num)),
+        AVG(CIT.amount * clothes.price) AS avg_spending,
+		GROUP_CONCAT(clothes.name ORDER BY clothes.name ASC) AS most_frequent_items
+FROM online_store.transactions T JOIN online_store.users ON users.email = T.email
+	JOIN online_store.clothes_in_transaction CIT ON T.order_num = CIT.order_num
+	JOIN online_store.clothes ON CIT.sku = clothes.sku
+    LEFT JOIN most_frequent ON CIT.sku = most_frequent.sku
+GROUP BY users.faculty, users.age, users.sex
+ORDER BY users.faculty, users.age,users.sex
+;
 
 
 -- campaign impact 
@@ -54,3 +75,40 @@ JOIN online_store.clothes_in_transaction cit ON t.order_num = cit.order_num
 JOIN online_store.clothes c ON c.sku = cit.sku;
 
 
+-- queries for visualizations:
+-- most popular items per gender
+SELECT users.sex, CIT.sku,  clothes.name ,COUNT(CIT.sku) AS num_purchases
+FROM online_store.transactions T
+JOIN online_store.users
+ON users.email = T.email
+JOIN online_store.clothes_in_transaction CIT
+ON T.order_num = CIT.order_num
+JOIN online_store.clothes
+ON CIT.sku = clothes.sku
+GROUP BY users.sex, CIT.sku
+HAVING users.sex = 0
+ORDER BY users.sex, num_purchases DESC;
+
+-- number of items per gender
+SELECT users.sex, SUM(CIT.amount) AS total_items_num
+FROM online_store.transactions T
+JOIN online_store.users
+ON users.email = T.email
+JOIN online_store.clothes_in_transaction CIT
+ON T.order_num = CIT.order_num
+JOIN online_store.clothes
+ON CIT.sku = clothes.sku
+GROUP BY users.sex
+ORDER BY users.sex;
+
+-- avg num of items per gender (per client)
+SELECT users.sex, AVG(CIT.amount) AS avg_items_num
+FROM online_store.transactions T
+JOIN online_store.users
+ON users.email = T.email
+JOIN online_store.clothes_in_transaction CIT
+ON T.order_num = CIT.order_num
+JOIN online_store.clothes
+ON CIT.sku = clothes.sku
+GROUP BY users.sex
+ORDER BY users.sex;
