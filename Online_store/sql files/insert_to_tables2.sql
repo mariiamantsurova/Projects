90d3865304bca749cf35a95f845dc21bd0efce83
Michelle Rudinsky <michellerudinsky@Michelles-MacBook-Air.local>
2025-01-28 14:45:34 +0200

    updated tables

--- Online_store/sql files/insert_to_tables2.sql
+++ Online_store/sql files/insert_to_tables2.sql
@@ -145,17 +145,17 @@ INSERT INTO online_store.transactions (order_num, date, hour, email) VALUES
 -- Insert into clothes
 INSERT INTO online_store.clothes (sku, name, price, available_amount, is_promoted, img_path)
 VALUES
-(10, 'Basic White T-Shirt', 15, 100, FALSE, '/images/basic_white_tshirt.jpg'),
-(11, 'Black Graphic T-Shirt', 20, 80, TRUE, '/images/black_graphic_tshirt.jpg'),
-(12, 'Cotton Crew Neck T-Shirt', 18, 50, FALSE, '/images/cotton_crew_neck_tshirt.jpg'),
-(13, 'V-Neck T-Shirt', 17, 60, TRUE, '/images/v_neck_tshirt.jpg'),
-(14, 'Slim Fit Blue Jeans', 40, 120, FALSE, '/images/slim_fit_blue_jeans.jpg'),
-(15, 'Black Skinny Jeans', 45, 70, TRUE, '/images/black_skinny_jeans.jpg'),
-(16, 'Light Wash Denim Jeans', 35, 90, FALSE, '/images/light_wash_denim_jeans.jpg'),
-(17, 'Straight Leg Jeans', 38, 65, FALSE, '/images/straight_leg_jeans.jpg'),
-(18, 'Classic Leather Jacket', 120, 40, TRUE, '/images/classic_leather_jacket.jpg'),
-(19, 'Denim Jacket', 60, 55, FALSE, '/images/denim_jacket.jpg'),
-(20, 'Puffer Jacket', 80, 30, TRUE, '/images/puffer_jacket.jpg');
+(10, 'Basic White T-Shirt', 15, 100, FALSE, 'https://xcdn.next.co.uk/COMMON/Items/Default/Default/ItemImages/AltItemShot/315x472/235459s2.jpg'),
+(11, 'Black T-Shirt', 20, 80, TRUE, 'https://vibez-store.com/wp-content/uploads/2024/11/tshirt-1.jpg'),
+(12, 'Sweater', 18, 50, FALSE, 'https://m.media-amazon.com/images/I/61DIDk2kiML._AC_SX679_.jpg'),
+(13, 'V-Neck T-Shirt', 17, 60, TRUE, 'https://m.media-amazon.com/images/I/61vqJXW3PUL._AC_SX679_.jpg'),
+(14, 'Slim Fit Blue Jeans', 40, 120, FALSE, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZjV6VCxaorzk8zzFbgBf3rxvyUg2DZyqtfw&s'),
+(15, 'Black Skinny Jeans', 45, 70, TRUE, 'https://cherryla.com/cdn/shop/products/BlackJeanBack_2048x.jpg?v=1680865591'),
+(16, 'Dress', 35, 90, FALSE, 'https://m.media-amazon.com/images/I/51ywkd-yHpL._AC_.jpg'),
+(17, 'Jacket', 38, 65, FALSE, 'https://cdn-images.farfetch-contents.com/24/09/94/78/24099478_54252248_600.jpg'),
+(18, 'Classic Leather Jacket', 120, 40, TRUE, 'https://www.schottnyc.com/images/800x800/519_BLK_FNT_NEW1.jpg'),
+(19, 'Denim Jacket', 60, 55, FALSE, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNxGUDHFbfV86N7mSE97jxm8r-FPioPEhVww&s'),
+(20, 'Puffer Jacket', 80, 30, TRUE, 'https://m.media-amazon.com/images/I/61P3PdkkMuL._AC_SY879_.jpg');
 
 
 -- Insert into clothes_in_transaction
